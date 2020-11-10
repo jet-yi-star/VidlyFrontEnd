d6d7be21d585961c2a5586039caa2c542269788d
Jet Yi <72280090+jet-yi-star@users.noreply.github.com>
2020-11-10 17:43:38 -0500

    Update movie.jsx

--- Component/movie.jsx
+++ Component/movie.jsx
@@ -1,29 +1,37 @@
 import React, { Component } from "react";
 import { deleteMovie, getMovie, getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 class Movie extends Component {
-  state = { movies: getMovies() };
+  state = { movies: getMovies(), tags: ["tag1", "tag2", "tag3"] };
 
   deleteMovieById = (id) => {
-    const movies = this.state.movies.filter(m=>m._id !== id);
+    let movieInDb = deleteMovie(id);
     // this.state.movies.splice(this.state.movies.indexOf(movieInDb), 1);
     // console.log(movieInDb);
-    this.setState({ movies});
+    this.setState({ movies: this.state.movies });
   };
   renderMessages() {
     if (this.state.movies.length === 0) return;
   }
+
+  handleLike = (movie) => {
+    console.log(movie);
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
   render() {
-       const {length: count} = this.state.movies;
     return (
       <React.Fragment>
-        
-        {count === 0 && (
+        {this.state.movies.length === 0 && (
           <span>There are no movies in the database</span>
         )}
-        {count !== 0 && (
-          <span>{`Showing ${count} movies in the database`}</span>
+        {this.state.movies.length !== 0 && (
+          <span>{`Showing ${this.state.movies.length} movies in the database`}</span>
         )}
-        {count !== 0 && (
+        {this.state.movies.length !== 0 && (
           <table className="table">
             <thead>
               <tr>
@@ -32,23 +40,32 @@ class Movie extends Component {
                 <th>Stock</th>
                 <th>Rate</th>
                 <th></th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
               {this.state.movies.map((it) => (
                 <tr key={it._id}>
-                  <td>{it.title}</td>
-                  <td>{it.genre.name}</td>
-                  <td>{it.numberInStock}</td>
-                  <td>{it.dailyRentalRate}</td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => {
-                      this.deleteMovieById(it._id);
-                    }}
-                  >
-                    Delete
-                  </button>
+                  <td key={it.title}>{it.title}</td>
+                  <td key={it.genre.name}>{it.genre.name}</td>
+                  <td key={it.numberInStock}>{it.numberInStock}</td>
+                  <td key={it.dailyRentalRate}>{it.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={it.liked}
+                      onClick={() => this.handleLike(it)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => {
+                        this.deleteMovieById(it._id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
